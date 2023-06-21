58736cbd7e5a4636e77f82fdbabf77a1686edcc1
stevenAnto <ecalcinap@unsa.edu.pe>
2023-06-20 21:23:45 -0500

    sintaxis error

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
 import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App(){
   return (
-    <article  className='tw-followCard' >
-      <header className='tw-followCard-header'>
-        <img className='tw-followCard-avatar' src="https://unavatar.io/github/37t?fallback=https://avatars.githubusercontent.com/u/66378906?v=4" />
-        <div className='tw-followCard-info'>
-          <strong>MiguelAngel</strong>
-          <span className='tw-followCard-infoUserName'>@sdfasf</span>
-        </div>
-      </header>
-      <aside>
-        <button>
-          Seguir
-        </button>
-      </aside>
-    </article>
+    <TwitterFollowCard />
   )
 }
---
+++ src/TwitterFollowCard.jsx
@@ -0,0 +1,18 @@
+export function TwitterFollowCard({userName, name, isFollowing}){
+  return (
+    <article  className='tw-followCard' >
+      <header className='tw-followCard-header'>
+        <img className='tw-followCard-avatar' src="https://unavatar.io/github/37t?fallback=https://avatars.githubusercontent.com/u/66378906?v=4" />
+        <div className='tw-followCard-info'>
+          <strong>{name}</strong>
+          <span className='tw-followCard-infoUserName'>@{userName}</span>
+        </div>
+      </header>
+      <aside>
+        <button>
+          Seguir
+        </button>
+      </aside>
+    </article>
+  )
+}
