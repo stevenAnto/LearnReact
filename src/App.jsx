0b10e69acf9723375f058d63741110a2b8f4e24e
stevenAnto <ecalcinap@unsa.edu.pe>
2023-06-20 21:08:17 -0500

    agregando estilos a un componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import './App.css'
+
 export function App(){
   return (
-    <article>
-      <header>
-        <img src="https://unavatar.io/github/37t?fallback=https://avatars.githubusercontent.com/u/66378906?v=4" />
-        <div>
+    <article  className='tw-followCard' >
+      <header className='tw-followCard-header'>
+        <img className='tw-followCard-avatar' src="https://unavatar.io/github/37t?fallback=https://avatars.githubusercontent.com/u/66378906?v=4" />
+        <div className='tw-followCard-info'>
           <strong>MiguelAngel</strong>
-          <span>@sdfasf</span>
+          <span className='tw-followCard-infoUserName'>@sdfasf</span>
         </div>
       </header>
       <aside>
