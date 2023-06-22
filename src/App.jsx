a42ea4a20ed28de9570e9573c90f61b7d767576a
stevenAnto <ecalcinap@unsa.edu.pe>
2023-06-22 17:03:35 -0500

    agregue una funcion que agrega el arroba, de manera tradicional no aplicando programacion funcional

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App(){
+  const addAt = (userName) => '@${userName}'
   return (
     <>
-    <TwitterFollowCard userName="Esteven Calcinan" name="stevenAnto" />
+    <TwitterFollowCard isFollowing={true} userName="Esteven Calcinan" name="stevenAnto" />
     <TwitterFollowCard userName=" Heneyr Isas" name="ElImbetiable" />
     <TwitterFollowCard userName=" Antony Aco" name="Atn" />
     </>
