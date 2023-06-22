66d0b62053c4242c4e12907917a9765ea922c5fd
stevenAnto <ecalcinap@unsa.edu.pe>
2023-06-22 17:10:58 -0500

    haciendo un call back en React

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App(){
-  const addAt = (userName) => '@${userName}'
+  const formatUsername= (userName)=> `@${userName}`
   return (
     <>
-    <TwitterFollowCard isFollowing={true} userName="Esteven Calcinan" name="stevenAnto" />
-    <TwitterFollowCard userName=" Heneyr Isas" name="ElImbetiable" />
-    <TwitterFollowCard userName=" Antony Aco" name="Atn" />
+      <TwitterFollowCard formatUsername={formatUsername} isFollowing={true} userName="Esteven Calcinan" name="stevenAnto" />
+      <TwitterFollowCard formatUsername={formatUsername} userName=" Heneyr Isas" name="ElImbetiable" />
+      <TwitterFollowCard formatUsername={formatUsername} userName=" Antony Aco" name="Atn" />
     </>
   )
 }
--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,12 +1,11 @@
-export function TwitterFollowCard({userName, name, isFollowing}){
-  const addAt = (userName) => '@${userName}'
+export function TwitterFollowCard({formatUsername, userName, name, isFollowing}){
   return (
     <article  className='tw-followCard' >
       <header className='tw-followCard-header'>
         <img className='tw-followCard-avatar' src="https://unavatar.io/github/37t?fallback=https://avatars.githubusercontent.com/u/66378906?v=4" />
         <div className='tw-followCard-info'>
           <strong>{name}</strong>
-          <span className='tw-followCard-infoUserName'>{addAt(userName)}</span>
+          <span className='tw-followCard-infoUserName'>{formatUsername(userName)}</span>
         </div>
       </header>
       <aside>
